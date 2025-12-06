9026ed2b3f006eab9840eccb1ee1c1006ca75b78
vishal123roy <150947504+vishal123roy@users.noreply.github.com>
2025-12-06 18:12:38 +0530

    server

--- api/result.js
+++ api/result.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const  payload  = req.body;
-  console.log(payload);
+  console.log(typeof payload);
   res.status(200).json({ message: "Hello from Vercel Serverless Function!" });
 }
 
